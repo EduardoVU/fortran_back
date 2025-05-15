6196669a7a54541817ff26a9fcb9ccb82d34c525
EduardoVU <eduardo.ur97@gmail.com>
2025-05-15 14:01:43 -0700

    Feat: correction

--- entities/fortran/router.py
+++ entities/fortran/router.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, UploadFile, File
 from typing import List
 from . import controller
+from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
 @router.post("/fortran")
 async def create_fortran(files: List[UploadFile] = File(...)):
-    return controller.create_formulario(files)
+    result = controller.create_formulario(files)
+    return JSONResponse(content=result)
